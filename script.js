4bd6bf0532f85a65f5ae45079f4fc2a8509c5ab4
Sumit saurav <117914094+sumitsaurav1@users.noreply.github.com>
2023-04-11 18:51:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,19 +12,18 @@ const lookup = {
 
 function rot13(encodedStr){
    let decodedArr = []; 
-	const words=encodedStr.split(" ");
-	for(let i=0;i<words.length;i++){
-		const word=words[i];
-		let decoded='';
-		for(let j=0;j<word.length;j++){
-			const char=word.charAt(j);
-			let decoded_char=lookup[char];
-			decoded+=decoded_char;
-			
+	for(let i=0;i<encodedStr.length;i++)
+		{
+			let cc= encodedStr.charAt(i)
+			if(cc==" "){
+				decodedArr.push(" ")
+			}
+			else{
+			    decodedArr.push(lookup[cc])
+			}
 		}
-		decodedArr.push(decoded);
-	}
-  return decodedArr.join(" ");//return decodedArr
+	
+  return decodedArr.join("");//return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
